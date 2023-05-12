25736edd69fef9b9006d9c21632ddb96ab3fac39
pokémféze <maximefeze1@gmail.com>
2023-05-12 18:12:45 +0100

    interface share cv

--- app/src/main/java/com/example/bettercvapp/screens/AddScreen/Share.kt
+++ app/src/main/java/com/example/bettercvapp/screens/AddScreen/Share.kt
@@ -1,16 +1,19 @@
 package com.example.bettercvapp.screens.AddScreen
 
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.*
-import androidx.compose.material.Icon
-import androidx.compose.material.Surface
-import androidx.compose.material.Text
-import androidx.compose.material.TextButton
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.*
 import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Share
 import androidx.compose.material.icons.rounded.ArrowBack
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
@@ -24,10 +27,173 @@ fun ShareCv(navController: NavController){
     Box(modifier = Modifier.fillMaxSize()) {
         Column() {
             head(navController)
-            Text(text = "Share your CV")
+            Surface( ) {
+                Box() {
+                    Row(
+                        Modifier
+                            .fillMaxWidth()
+                            .padding(horizontal = 8.dp),
+                        verticalAlignment = Alignment.CenterVertically,
+                        horizontalArrangement = Arrangement.Center,
+                    ) {
+                        Text(
+                            text = "SHARE YOUR CV",
+                            fontFamily = Poppins,
+                            color = Color.Black,
+                            fontSize = 25.sp,
+                        )
+
+                    }
+                }
+
+            }
+
+            Spacer(modifier = Modifier.height(30.dp))
+
+            Surface(
+                modifier = Modifier.fillMaxWidth(),
+                shape = RoundedCornerShape(16.dp),
+                elevation = 4.dp
+            ) {
+                Box() {
+                    Row(
+                        modifier = Modifier.padding(16.dp)
+                    ) {
+                        Image(
+                            painter = painterResource(id = R.drawable.developpeur),
+                            contentDescription = null,
+                            contentScale = ContentScale.Crop,
+                            modifier = Modifier
+                                .size(96.dp)
+                                .clip(RoundedCornerShape(8.dp))
+                        )
+                        Spacer(modifier = Modifier.width(16.dp))
+                        Column {
+                            Text(
+                                text = "Cv devellopeur back-end",
+                                style = MaterialTheme.typography.h6
+                            )
+                            Spacer(modifier = Modifier.height(8.dp))
+                            Text(
+                                text = "Je devellope en Java et C#",
+                                style = MaterialTheme.typography.body1
+                            )
+                        }
+                        IconButton(
+                            onClick = { /* Handle share button click */ },
+                            modifier = Modifier
+                                .align(Alignment.CenterVertically)
+                                .padding(start = 4.dp, end = 10.dp)
+                        ) {
+                            Icon(
+                                imageVector = Icons.Default.Share,
+                                contentDescription = "Share"
+                            )
+                        }
+                    }
+                }
+            }
+
+            Spacer(modifier = Modifier.height(10.dp))
+
+            Surface(
+                modifier = Modifier.fillMaxWidth(),
+                shape = RoundedCornerShape(16.dp),
+                elevation = 4.dp
+            ) {
+                Box() {
+                    Row(
+                        modifier = Modifier.padding(16.dp)
+                    ) {
+                        Image(
+                            painter = painterResource(id = R.drawable.devellopeur),
+                            contentDescription = null,
+                            contentScale = ContentScale.Crop,
+                            modifier = Modifier
+                                .size(96.dp)
+                                .clip(RoundedCornerShape(8.dp))
+                        )
+                        Spacer(modifier = Modifier.width(16.dp))
+                        Column {
+                            Text(
+                                text = "Cv devellopeur front-end",
+                                style = MaterialTheme.typography.h6
+                            )
+                            Spacer(modifier = Modifier.height(8.dp))
+                            Text(
+                                text = "Je devellope en Java et C#",
+                                style = MaterialTheme.typography.body1
+                            )
+                        }
+                        IconButton(
+                            onClick = { /* Handle share button click */ },
+                            modifier = Modifier
+                                .align(Alignment.CenterVertically)
+                                .padding(start = 4.dp, end = 10.dp)
+                        ) {
+                            Icon(
+                                imageVector = Icons.Default.Share,
+                                contentDescription = "Share"
+                            )
+                        }
+                    }
+                }
+            }
+
+            Spacer(modifier = Modifier.height(10.dp))
+            Surface(
+                modifier = Modifier.fillMaxWidth(),
+                shape = RoundedCornerShape(16.dp),
+                elevation = 4.dp
+            ) {
+                Box() {
+                    Row(
+                        modifier = Modifier.padding(16.dp)
+                    ) {
+                        Image(
+                            painter = painterResource(id = R.drawable.analyste),
+                            contentDescription = null,
+                            contentScale = ContentScale.Crop,
+                            modifier = Modifier
+                                .size(96.dp)
+                                .clip(RoundedCornerShape(8.dp))
+                        )
+                        Spacer(modifier = Modifier.width(16.dp))
+                        Column {
+                            Text(
+                                text = "Chef De Projet",
+                                style = MaterialTheme.typography.h6
+                            )
+                            Spacer(modifier = Modifier.height(8.dp))
+                            Text(
+                                text = "Apte a gerer plusieur projet",
+                                style = MaterialTheme.typography.body1
+                            )
+                        }
+                        IconButton(
+                            onClick = { /* Handle share button click */ },
+                            modifier = Modifier
+                                .align(Alignment.CenterVertically)
+                                .padding(start = 40.dp, end = 10.dp)
+                        ) {
+                            Icon(
+                                imageVector = Icons.Default.Share,
+                                contentDescription = "Share"
+                            )
+                        }
+                    }
+                }
+            }
+
         }
     }
 }
+
+
+
+
+
+
 
 
 @Composable
