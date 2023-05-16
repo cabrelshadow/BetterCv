df82c5ac9d1e3fca0911d00d2e701956cdcdd9b7
peughouia <peughouia.44@gmail.com>
2023-05-16 17:43:42 +0100

    mise a jour

--- app/src/main/java/com/example/bettercvapp/screens/AddScreen/AddExperiencePro.kt
+++ app/src/main/java/com/example/bettercvapp/screens/AddScreen/AddExperiencePro.kt
@@ -89,7 +89,60 @@ fun ProfessionalExpScreen(navController: NavController, context : Context) {
     
     Scaffold(
 
-        bottomBar = { BottomBar() },
+        bottomBar = {
+            Surface(color = Color.White,
+                elevation = 10.dp) {
+                Row(
+                    Modifier
+                        .fillMaxWidth()
+                        .height(100.dp),
+                    horizontalArrangement = Arrangement.SpaceAround,
+                    verticalAlignment = Alignment.CenterVertically
+                ) {
+                    Button(
+                        onClick = {
+                            startDate = date.value
+                            endDate = date2.value
+                            saveExpro()
+                        },
+                        Modifier
+                            .height(40.dp)
+                            .width(115.dp)
+                            .align(Alignment.CenterVertically),
+                        //shape = InputBoxShape.medium,
+                        shape = MyShape,
+                    )
+                    {
+                        Icon(Icons.Rounded.ArrowDropDown, contentDescription = "")
+                        Text(
+                            text = "Save",
+                            color = Color.White,
+                            fontSize = 15.sp,
+                            fontFamily = Poppins
+                        )
+                    }
+                    //add button
+                    Button(
+                        onClick = { /*TODO*/ },
+                        Modifier
+                            .height(40.dp)
+                            .width(115.dp)
+                            .align(Alignment.CenterVertically),
+                        shape = MyShape
+                    )
+                    {
+                        Icon(Icons.Rounded.Add, contentDescription = "")
+                        Text(
+                            text = "Add",
+                            color = Color.White,
+                            fontSize = 15.sp,
+                            fontFamily = Poppins
+                        )
+                    }
+                }
+            }
+
+        },
         content = {it
             LazyColumn( contentPadding = PaddingValues(bottom = 40.dp)){
                 stickyHeader {
